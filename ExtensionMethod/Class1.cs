461da13a741975c4c8bdf590bff774c2edd938b6
NavaneethKaila <navaneeth.kaila@gmail.com>
2022-08-22 11:30:42 +0530

    Add project files.

---
+++ ExtensionMethod/Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ExtensionMethod
+{
+        public class Extension
+        {
+            public void Currency_Valid()
+            {
+                string currency = "123";
+                Assert.Equal("$123", ExtensionMethodClass.ToCurrency(currency));
+            }
+            [InlineData("123abc")]
+            [InlineData("abcd9010")]
+            public void Currency_InValid(string currency)
+            {
+                string excep = "Entered String does not contain numbers";
+                var exp = Assert.Throws<ArgumentException>(() => ExtMethodClass.ToCurrency(currency));
+                Assert.Equal(excep, exp.Message);
+            }
+            public void Currency_EmptyString()
+            {
+                string currency = "";
+                Assert.Equal("$", ExtMethodClass.ToCurrency(currency));
+            }
+    }
+}
